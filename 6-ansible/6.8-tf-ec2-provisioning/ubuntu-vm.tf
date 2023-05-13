6c1ad08d05d3dcd5e10e8509aaa4637a4c95d80e
Varun manik <sl-lab-vm@example.com>
2023-05-13 04:54:33 +0000

    docker commands

--- 6-ansible/6.8-tf-ec2-provisioning/ubuntu-vm.tf
+++ 6-ansible/6.8-tf-ec2-provisioning/ubuntu-vm.tf
@@ -32,7 +32,7 @@ resource "aws_instance" "ubuntu" {
   ami                    = "ami-007855ac798b5175e"
   instance_type          = "t3.micro"
   key_name               = aws_key_pair.deployer.key_name
-  count                  = 2
+  count                  = 3
   vpc_security_group_ids = ["${aws_security_group.allow_SSH_ubuntu.id}"]
   tags = {
     "Name" = "UBUNTU-${count.index}"
