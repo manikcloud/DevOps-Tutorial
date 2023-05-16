30680a3fffd8aea671b8bb72467d260331de34a2
Pinkysahoo43 <nicepinky.sahoo43@gmail.com>
2023-05-16 16:31:23 +0000

    docker commands

--- 6-ansible/6.8-tf-ec2-provisioning/ubuntu-vm.tf
+++ 6-ansible/6.8-tf-ec2-provisioning/ubuntu-vm.tf
@@ -33,8 +33,9 @@ resource "aws_instance" "ubuntu" {
   instance_type          = "t3.micro"
   key_name               = aws_key_pair.deployer.key_name
   vpc_security_group_ids = ["${aws_security_group.allow_SSH_ubuntu.id}"]
+  count                  = 3
   tags = {
-    "Name" = "UBUNTU-16-05"
+    "Name" = "UBUNTU-${count.index}"
     "ENV"  = "Dev"
   }
 
