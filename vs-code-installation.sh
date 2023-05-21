702f3ec96b0824dce8a79ba7a182347a47b7a50f
Kumar <varkumar@deloitte.com>
2023-05-21 09:30:40 +0800

    compose

--- vs-code-installation.sh
+++ vs-code-installation.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo apt update
-sudo apt install software-properties-common  apt-transport-https wget
+sudo apt install software-properties-common  apt-transport-https wget -y
 wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
-sudo apt install code
+sudo apt install code -y
