539dae2333188c813d70db6f16312eaefe598ab3
varun-sl <sl@example.com>
2023-05-21 04:26:44 +0000

    readme

--- vs-code-installation.sh
+++ vs-code-installation.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo apt update
+sudo apt update -y
 sudo apt install software-properties-common  apt-transport-https wget -y
 wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
