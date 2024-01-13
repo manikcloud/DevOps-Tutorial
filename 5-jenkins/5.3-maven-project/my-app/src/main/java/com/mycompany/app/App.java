a247ab1ea124eedc643ed841405f8e9ac8dd5f65
varun-sl <varun@gmail.com>
2024-01-13 07:20:00 +0000

    adding date

--- 5-jenkins/5.3-maven-project/my-app/src/main/java/com/mycompany/app/App.java
+++ 5-jenkins/5.3-maven-project/my-app/src/main/java/com/mycompany/app/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! From Varun Manik in Simplilearn class on dat Jan 13 2024" );
+        System.out.println( "Hello World! From Varun Manik in Simplilearn class on date Jan 13 2024" );
     }
 }
