fc82b1860b752f98a4b04f59041c49511e5bace7
varun-sl <varun@gmail.com>
2024-01-13 07:18:18 +0000

    adding date

--- 5-jenkins/5.2-simple-java-program/HelloWorld.java
+++ 5-jenkins/5.2-simple-java-program/HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
   public static void main(String[] args) {
-	System.out.println("Hello, World From Varun Manik on date May 6 2023");
+	System.out.println("Hello, World From Varun Manik on date MaJan 13 2024");
   }
 }
--- 5-jenkins/5.3-maven-project/my-app/src/main/java/com/mycompany/app/App.java
+++ 5-jenkins/5.3-maven-project/my-app/src/main/java/com/mycompany/app/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! From Varun Manik in Simplilearn class" );
+        System.out.println( "Hello World! From Varun Manik in Simplilearn class on dat Jan 13 2024" );
     }
 }
