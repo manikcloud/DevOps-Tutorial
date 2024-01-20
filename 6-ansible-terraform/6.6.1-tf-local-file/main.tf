f154b9641449d13e1054af774d2252ff4fd68ec3
varun <v@gmail.com>
2024-01-20 03:54:44 +0000

    adding tf local

---
+++ 6-ansible-terraform/6.6.1-tf-local-file/main.tf
@@ -0,0 +1,7 @@
+resource "local_file" "index_file" {
+
+  content  = "Hi How are you ?"
+
+  filename = "index.html"
+
+}
---
+++ 6-ansible-terraform/6.7-S3-Bucket-Using-Terraform/creds.tf
@@ -0,0 +1,8 @@
+
+
+provider "aws" {
+  access_key = ""
+  secret_key = ""
+  token = ""
+  region = "us-east-1"
+}
