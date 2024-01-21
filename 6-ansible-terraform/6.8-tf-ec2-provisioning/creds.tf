9ae76e9f7d30c2ec0f9a181270e304e29b0fea88
varun <v@gmail.com>
2024-01-21 05:09:27 +0000

    adding changes

--- 6-ansible-terraform/6.8-tf-ec2-provisioning/creds.tf
+++ 6-ansible-terraform/6.8-tf-ec2-provisioning/creds.tf
@@ -1,6 +1,6 @@
 provider "aws" {
-  access_key = "AKIAQ7ZCMYMXWIQ45EDI"
-  secret_key = "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"
+  access_key = "AKIAQ7ZCMYMXTD7Q2FOK"
+  secret_key = "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"
   token = ""
   region = "us-east-1"
 }
